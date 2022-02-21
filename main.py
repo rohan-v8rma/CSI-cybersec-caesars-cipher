3a93bdab2dd95105683ba82b2901ac7f33bf2432
rohan-verma19 <rv725555@gmail.com>
2022-02-21 11:51:36 +0530

    improv: Eliminated redundant code and improved variable naming

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-input_string = list(input("Enter the Encoded String >> "))
+encoded_string = list(input("Enter the Encoded String >> "))
 
 for key in range(1, 26):
     decoded_string = []  
-    for char in input_string:
+    for char in encoded_string:
         if char.isalpha():
+            error_correction = 0 
+            # this variable ensures that if Z is incremented by 1 position, we end up with A 
+            # and not the ASCII character corresponding with 91
             if (char.islower() and ((ord(char) + key) > 122)) or (char.isupper() and ((ord(char) + key) > 90)):
-                decoded_string.append(chr(ord(char) + key - 26))
-            else:
-                decoded_string.append(chr(ord(char) + key))
+                error_correction = 26
+            decoded_string.append(chr(ord(char) + key - error_correction))
         else:
             decoded_string.append(char)
     print(''.join(decoded_string))            
